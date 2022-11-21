3439079023eb37af175874ce585c717c27201717
viboy196 <viboy196@gmail.com>
2022-11-21 11:50:37 +0700

    ok

--- utils/api/axios.ts
+++ utils/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = "https://thinkfood.vn:7683";
-//axios.defaults.baseURL = "https://0c73-2405-4803-ff26-3fd0-6e42-1131-5030-137f.ap.ngrok.io";
+//axios.defaults.baseURL = "https://49b9-2405-4803-fc60-cd80-34c7-8f99-a101-4f7d.ap.ngrok.io/";
 
 export default axios;
